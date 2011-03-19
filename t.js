9a89f08bdfc2a091d6eafc7f518e7a579aea7e7e
Paul Querna <pquerna@apache.org>
2011-03-19 16:42:45 -0700

    just a demo-ish script for testing right now

---
+++ t.js
@@ -0,0 +1,8 @@
+var logmagic = require('logmagic');
+var log = logmagic.local('mylib.foo.bar');
+console.log(log);
+log.info("Hello!");
+log.error("Accepts format strings too ${SOME_VAR}", {SOME_VAR: "myvalue"});
+log.trace("testing trace v0");
+logmagic.route("__root__", logmagic.TRACE1, "console");
+log.trace("testing trace v1");
