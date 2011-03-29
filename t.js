d3f54c5d4a2e5e39d58648937c8b8a3c90f2e7aa
Paul Querna <pquerna@apache.org>
2011-03-29 09:44:47 -0700

    Log the object thats passed in

--- t.js
+++ t.js
@@ -5,4 +5,5 @@ log.info("Hello!");
 log.error("Accepts format strings too ${SOME_VAR}", {SOME_VAR: "myvalue"});
 log.trace("testing trace v0");
 logmagic.route("__root__", logmagic.TRACE1, "console");
-log.trace("testing trace v1");
+log.trace("testing trace v1", {slug: 1});
+console.log(log);
