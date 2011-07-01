e7c6374edc6175ce17cb9e5230df2fccfe7f1226
Paul Querna <pquerna@apache.org>
2011-07-01 15:11:24 -0700

    Add rewriters to logmagic, letting you pass in a generic thing like the request object, and pick out things to log on it, rather than the whole object

--- tests/t.js
+++ tests/t.js
@@ -6,11 +6,22 @@ log.error("more stuff", {SOME_VAR: "myvalue"});
 log.trace("testing trace v0");
 
 logmagic.route("__root__", logmagic.TRACE1, "console");
-//logmagic.route("__root__", logmagic.TRACE1, "graylog2-stderr");
+logmagic.route("__root__", logmagic.TRACE1, "graylog2-stderr");
 log.trace("testing trace v1", {slug: 1});
 
 log = logmagic.local('mylib.foo.cars');
 log.trace("hello world", {counter: 33, account_id: 42, txnid: "fxxxxx"});
+logmagic.addRewriter(function(modulename, level, msg, extra) {
+  if (extra.request) {
+    extra.accountId = extra.request.account.id;
+    extra.txnId = extra.request.txtId;
+    delete extra.request;
+  }
+  return extra;
+});
+
 log.trace("hello baksdfnsdf", {special: 'aaa', account_id: 42, txnid: "fxxxxx", full_message: "loooong message"});
+
+log.dbg("hello xxxx", {request: {account: {id: 45}, txtId: "XXXXXXXXXXXXX"}});
 
 //console.log(log);
